37673ca9032f5d4bf43cb109220739c283c5a89f
Gonçalo Valério <gon@ovalerio.net>
2018-02-22 16:21:05 +0000

    added example smart contract just to start

---
+++ contracts/neo-vitae.py
@@ -0,0 +1,22 @@
+"""Neo-vitae Smart-Contract
+
+This smart-contract intents to register with the NEO blockchain all user
+certificates, diplomas, workplaces and all other achievements during their
+lifetime.
+
+Other entities are able to certify that a given address owner has done
+something in a permanent way.
+
+Usage:
+
+- Add later
+
+"""
+from boa.blockchain.vm.Neo.Runtime import Log, Notify
+from boa.blockchain.vm.Neo.Runtime import CheckWitness
+from boa.blockchain.vm.Neo.Storage import GetContext, Get, Put, Delete
+
+
+def Main():
+    Log("Neo-Vitae")
+    Notify("We are just getting started")
