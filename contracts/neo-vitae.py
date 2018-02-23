f449262f3555c815dbd1a9505bd0f15e3e2d5955
Gonçalo Valério <gon@ovalerio.net>
2018-02-23 15:14:47 +0000

    contract now returns all the results

--- contracts/neo-vitae.py
+++ contracts/neo-vitae.py
@@ -22,9 +22,8 @@ Usage:
 """
 from boa.blockchain.vm.Neo.Runtime import Log, Notify
 from boa.blockchain.vm.Neo.Runtime import CheckWitness
-from boa.blockchain.vm.Neo.Storage import GetContext, Get, Put
 from boa.blockchain.vm.System.ExecutionEngine import GetCallingScriptHash
-from boa.code.builtins import concat
+from boa.code.builtins import concat, substr
 from utils.storage import StorageManager
 
 
@@ -32,7 +31,7 @@ def Main(operation, *args):
 
     if len(args) == 0:
         Log('You need to provide at least 1 parameter - [address]')
-        return ''
+        return []
 
     address = args[0]
 
@@ -42,40 +41,48 @@ def Main(operation, *args):
         # Caller cannot add certifications to his address
         if CheckWitness(address):
             Log('You cannot add certitications for yourself')
-            return ''
+            return []
         if 1 == len(args):
             Log('To certify 2 parameters are needed - [address] [content]')
-            return ''
+            return []
         content = args[1]
         return add_certification(address, content)
     else:
         Log('Invalid Operation')
-        return ''
+        return []
 
 
 def get_certs(address):
     store = StorageManager()
     current_data = store.get(address)
     if current_data:
-        Notify(current_data)
+        entries = store.deserialize_bytearray(current_data)
+        final = []
+        for entry in entries:
+            sender_address = substr(entry, 0, 34)
+            length = len(entry) - 34
+            content = substr(entry, 34, length)
+            final.append([sender_address, content])
+        Notify(final)
     else:
         Notify('No certifications found for this address')
-    return current_data
+    return final
 
 
 def add_certification(address, content):
     sender = GetCallingScriptHash()
     store = StorageManager()
     current_data = store.get(address)
+    new_entry = concat(sender, content)
 
     if not current_data:
-        new_data = concat(sender, content)
+        new_data = [new_entry]
     else:
         current_data = store.deserialize_bytearray(current_data)
-        new_entry = concat(sender, content)
         current_data.append(new_entry)
         new_data = current_data
 
-    store.put(address, store.serialize_array(new_data))
+    final_data = store.serialize_array(new_data)
+    store.put(address, final_data)
     Notify('New certification added.', address, content)
-    return new_data
+    return final_data
